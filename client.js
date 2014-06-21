26caafdd501f30e19e1b4adcfded07cf5e892749
scrivy <scrivinator@gmail.com>
2014-06-21 13:48:31 -0700

    added broadcast

--- client.js
+++ client.js
@@ -3,22 +3,22 @@
 var dgram = require('dgram');
 
 
-var server = dgram.createSocket('udp4');
+var client = dgram.createSocket('udp4');
 
-server.on('error', function(err) {
-  console.log('server error:\n' + err.stack);
-  server.close();
+client.on('error', function(err) {
+  console.log('client error:\n' + err.stack);
+  client.close();
 });
 
-server.on('message', function(msg, rinfo) {
-  console.log('server got: ' + msg + ' from ' +
+client.on('message', function(msg, rinfo) {
+  console.log('client got: ' + msg + ' from ' +
     rinfo.address + ':' + rinfo.port);
 });
 
-server.on('listening', function() {
-  var address = server.address();
-  console.log('server listening ' +
+client.on('listening', function() {
+  var address = client.address();
+  console.log('client listening ' +
     address.address + ':' + address.port);
 });
 
-server.bind(4000);
+client.bind(4000);
